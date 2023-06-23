59a0fe637b4602f490664ff924ec2575bff2f294
theba <thebaddestmatty@outlook.com>
2023-06-23 15:41:35 +0100

    leaderboard table

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Matthew Wilson';
+const name = 'Leaderboard';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/fiesta.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -43,7 +43,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/images/profile.jpg"
+                src="/images/fiesta.jpg"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
